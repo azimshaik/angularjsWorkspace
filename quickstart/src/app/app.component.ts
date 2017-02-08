61af5324a0e68311b20469947f3ae831faa11f46
azimshaik <cheer.buddu@gmail.com>
2017-02-08 12:19:07 -0500

    INTERPOLATION
    {{hero}} and {{title}} tells our app to read the title and hero properties from the component and render them---tthis so called INTERPOLATION

--- quickstart/src/app/app.component.ts
+++ quickstart/src/app/app.component.ts
@@ -4,8 +4,9 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template: `<h1>{{title}}</h1><h2>{{hero}} details!</h2>`,
 })
+//{{hero}} and {{title}} tells our app to read the title and hero properties from the component and render them---tthis so called "INTERPOLATION"
 export class AppComponent  { 
 	name = 'Angular'; 
-	title= 'Tour of heros';
-	hero='Windstorm';
+	title= 'Tour of heroes';
+	hero='Windstormmmmmmm!!!!';
 }
