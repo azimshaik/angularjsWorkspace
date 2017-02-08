ad7e48d334b657b77b1434a94f86faf6d07fe988
azimshaik <cheer.buddu@gmail.com>
2017-02-08 12:27:15 -0500

    Class Hero
    new Hero class , Refractored the components hero property from String to type Hero and initialized the id and name, also update the binding inthe template to refer to hero's name property

--- quickstart/src/app/app.component.ts
+++ quickstart/src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 
+export class Hero{
+	id: number;
+	name:string;
+}
+
 @Component({
   selector: 'my-app',
-  template: `<h1>{{title}}</h1><h2>{{hero}} details!</h2>`,
+  template: `<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>`,
 })
-//{{hero}} and {{title}} tells our app to read the title and hero properties from the component and render them---tthis so called "INTERPOLATION"
+/**{{hero}} and {{title}} tells our app to read the title and hero properties from the component and render them---tthis so called "INTERPOLATION"*/
+
 export class AppComponent  { 
 	name = 'Angular'; 
 	title= 'Tour of heroes';
-	hero='Windstormmmmmmm!!!!';
+	hero: Hero = {
+	id:1,
+	name:'Windstorm'
+	}
 }
