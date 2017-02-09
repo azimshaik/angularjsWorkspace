70a6f9f9d8451adce92397844676edb961ac8c82
azimshaik <cheer.buddu@gmail.com>
2017-02-08 19:23:14 -0500

    ngIf
    We wrap the HTML hero detail content of our template with a <div>. Then we add the ngIf built-in directive and set it to the selectedHero property of our component.

--- quickstart/src/app/app.component.ts
+++ quickstart/src/app/app.component.ts
@@ -25,10 +25,18 @@ const HEROES: Hero[] = [
   			<h1>{{title}}</h1>
   			<h2>My Heroes</h2>
 				<ul class="heroes">
-  					<li *ngFor="let hero of heroes">
+  					<li *ngFor="let hero of heroes" (click)="onSelect(hero)">
     					<span class="badge">{{hero.id}}</span>{{hero.name}}
   					</li>
 				</ul>
+            <div *ngIf="selectedHero">
+  				<h2>{{selectedHero.name}} details!</h2>
+  				<div><label>id: </label>{{selectedHero.id}}</div>
+  				<div>
+    				<label>name: </label>
+    				<input [(ngModel)]="selectedHero.name" placeholder="name"/>
+  				</div>
+			</div>
   			  `,
   styles: [`
   			.selected {
@@ -78,7 +86,7 @@ const HEROES: Hero[] = [
     			margin-right: .8em;
     			border-radius: 4px 0 0 4px;
   			}
-			`]
+		`]
 
 })
 /**{{hero}} and {{title}} tells our app to read the title and hero properties from the component and render them---tthis so called "INTERPOLATION"*/
@@ -87,5 +95,11 @@ export class AppComponent  {
 	name = 'Angular'; 
 	title= 'Tour of heroes';
 	heroes = HEROES;
+	selectedHero: Hero;
+
+	onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+
+  }
 }
 
