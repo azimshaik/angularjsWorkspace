4e367308e0720c2607eada4e7ff3a2dfa1e72f59
azimshaik <cheer.buddu@gmail.com>
2017-02-10 20:00:20 -0500

    HttpModule - routing

--- quickstart/src/app/app.component.ts
+++ quickstart/src/app/app.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
 
 
-export class Hero{
-	id: number;
-	name:string;
-}
 /** array of heroes */
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
@@ -20,23 +17,17 @@ const HEROES: Hero[] = [
   { id: 20, name: 'Tornado' }
 ];
 @Component({
+	/** configuration options*/
   selector: 'my-app',
   template: `
   			<h1>{{title}}</h1>
   			<h2>My Heroes</h2>
 				<ul class="heroes">
-  					<li *ngFor="let hero of heroes" (click)="onSelect(hero)">
+  					<li *ngFor="let hero of heroes" [class.selected]="hero === selectedHero"(click)="onSelect(hero)">
     					<span class="badge">{{hero.id}}</span>{{hero.name}}
   					</li>
 				</ul>
-            <div *ngIf="selectedHero">
-  				<h2>{{selectedHero.name}} details!</h2>
-  				<div><label>id: </label>{{selectedHero.id}}</div>
-  				<div>
-    				<label>name: </label>
-    				<input [(ngModel)]="selectedHero.name" placeholder="name"/>
-  				</div>
-			</div>
+			          
   			  `,
   styles: [`
   			.selected {
