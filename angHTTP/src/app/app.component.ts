cef8df2d23e3bcec21567f6e9c84ce8a855c50c2
azimshaik <cheer.buddu@gmail.com>
2017-02-11 21:05:21 -0500

    working on routing

---
+++ angHTTP/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {AboutComponent} from './components/about/about.component';
+
+@Component({
+  moduleId: module.id,
+  selector: 'my-app',
+  templateUrl: `app.component.html`,
+  
+})
+export class AppComponent  { name = 'Angular'; }
