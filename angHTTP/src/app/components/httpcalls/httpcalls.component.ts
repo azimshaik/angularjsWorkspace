b07e5858c95172add2cae96b0d2c4ded503532b4
azimshaik <cheer.buddu@gmail.com>
2017-02-14 17:48:23 -0500

    http still not working

--- angHTTP/src/app/components/httpcalls/httpcalls.component.ts
+++ angHTTP/src/app/components/httpcalls/httpcalls.component.ts
@@ -1,8 +1,42 @@
 import { Component } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Http, Response} from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+import {Posts} from './posts';
+@Injectable()
 
 @Component({
   moduleId:module.id,
   selector: 'httpcalls',
   templateUrl: `httpcalls.component.html`,
 })
-export class HttpcallsComponent  { name = 'Angular'; }
+export class HttpcallsComponent  { 
+    onClickMe(){
+        console.log("hit it hard");
+        let serviceurl = 'https://jsonplaceholder.typicode.com/posts';
+        console.log(serviceurl);
+        
+    }
+    posts = [
+        new Posts(1,1,'this is post one title','this is post body'),
+        new Posts(2,2,'this is post one title','this is post body')
+    ];
+    myposts = this.posts[0];
+
+}
+
+// name = 'Angular'; 
+//     private serviceurl = 'https://jsonplaceholder.typicode.com/posts';
+//     constructor (private http:Http){}
+//     getPosts():Observable<Posts[]>{
+//         return this.http.get(this.serviceurl)
+//                         .map(this.extractData)
+//                         ;
+//     }
+//     private extractData(res: Response){
+//         let body = res.json();
+//         console.log(body);
+//         return body.data || {};
+//     }
