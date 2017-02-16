300ac223b4fd661e36d59ce07b09ebf0e9f26568
azimshaik <cheer.buddu@gmail.com>
2017-02-15 22:09:18 -0500

    mongodb database connected  locall😛

--- meanauthapp/app.js
+++ meanauthapp/app.js
@@ -4,10 +4,39 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport  = require('passport-jwt');
 const mongoose = require('mongoose');
+const config = require('./config/database');
 
+//connect to database
+mongoose.connect(config.database);
+
+//on connection
+mongoose.connection.on('connected',() => {
+    console.log('Connected to database'+config.database);
+});
+mongoose.connection.on('error',() => {
+    console.log('Database error'+err);
+});
 const app = express();
-const port = 3002;
 
+const users = require('./routes/users');
+const port = 3002;
+//Cors middle wear to enable run server and client on same port
+app.use(cors());
+
+//Set static folder
+app.use(express.static(path.join(__dirname,'public')));
+
+//Body parser middleware
+app.use(bodyParser.json());
+
+app.use('/users', users);
+
+//Index route
+app.get('/', (req,res) =>{
+    res.send('Invalid end point');
+});
+
+//Start server
 app.listen(port, () => {
     console.log('Server started on port'+port);
 });
